0702043770d202da9bde5456027b7fd8ba6c2bf3
Banacount <uniquejmal@gmail.com>
2026-06-29 11:05:57 +0800

    A1: Dark theme and a of bit refactor

--- static/home_func.js
+++ static/home_func.js
@@ -4,27 +4,34 @@ const generateTempLobby = document.getElementById("generateTempLobby");
 const gotoGlobalLobby = document.getElementById("gotoGlobalLobby");
 const generatedLink = document.getElementById("generatedLink");
 
-userNameInput.addEventListener('input', (e) => {
+userNameInput.addEventListener('input', () => 
+{
 	saveUsername(userNameInput.value);
 });
 
-gotoGlobalLobby.addEventListener('click', () => {
-	window.location = encodeURI(`/chats?username=${loadUsername()}`);
+gotoGlobalLobby.addEventListener('click', () => 
+{
+	// Go to chats route
+	window.location = "/chats";
 });
 
-generateTempLobby.addEventListener('click', async () => {
+generateTempLobby.addEventListener('click', async () => 
+{
 	let lobby_name = prompt("Enter the desired lobby name: ");
 
 	if (lobby_name != null) {
-		if (lobby_name.trim() == "") alert("Invalid lobby name.");
+		if (lobby_name.trim() == "") 
+			alert("Invalid lobby name.");
 		else {
 			try {
-				const path = encodeURI(`/chats/new/${lobby_name}`);
-				const response = await fetch(path);
+				const requestLobbyLink = encodeURI(`/chats/new/${lobby_name}`);
+				const response = await fetch(requestLobbyLink);
 
 				if (!response.ok) throw Error('Error in receiving the data.');
 
+				// { message: [server message], link: [lobby generated link] }
 				const data = await response.json();
+
 				let isLink = false;
 				generatedLink.style.display = 'flex';
 
